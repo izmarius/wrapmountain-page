a59a738bac427cf64f67a7e4b5fe06ca9c519e6c
izmarius <izmarius.im@gmail.com>
2021-06-14 19:59:12 +0300

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colosrs: {
+        sunset: '#feb25e',
+        mountain: '#b2d0c6'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
